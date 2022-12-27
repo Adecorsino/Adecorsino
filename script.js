ae841fb15d580c75c5393ad51cc0f42218d2774a
Adecorsino <adecorsino@gmail.com>
2022-12-27 20:19:05 -0300

    dom3

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 let elementoJavaScript = document.createElement("li");
 elementoJavaScript.innerText = "JavaScript"
+elementoJavaScript.id = "ling-js"
+let listalinguagens = document.querySelector(".lista.linguagens");
+listalinguagens.appendchild(elementoJavaScript);
 console.log(elementoJavaScript);
