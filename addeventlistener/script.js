3cad4daea57a70afc6ca600573eb36fb12b663cb
Adecorsino <adecorsino@gmail.com>
2023-01-01 10:57:19 -0300

    botao2

--- addeventlistener/script.js
+++ addeventlistener/script.js
@@ -2,7 +2,7 @@ let numero = document.querySelector("h1");
 let botao = document.querySelector("button");
 let contagem = 0;
 function adicionarUm(){
-    contagem=contagem+1;
-    numero.innerText=contagem;
+    contagem = contagem + 1;
+    numero.innerText = contagem;
 }
-adicionarUm();
+botao.addEventListener("click",adicionarUm);
