250df5dee159e9b646525e00caf105b45537c0fd
Adecorsino <adecorsino@gmail.com>
2023-01-01 20:30:45 -0300

    botao5

--- addeventlistener/script.js
+++ addeventlistener/script.js
@@ -2,9 +2,7 @@ let numero = document.querySelector("h1");
 let botao = document.querySelector("button");
 let contagem = 0;
 
-botao.addEventListener("click",function(){
-    contagem = contagem + 1;
+botao.addEventListener("click",()=>{
+    contagem = contagem +1;
     numero.innerText = contagem;
 });
-
-
