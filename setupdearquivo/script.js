18bcc488b7743c89af2a5cb5934898092f579cf3
Adecorsino <adecorsino@gmail.com>
2023-01-01 10:05:13 -0300

    botao

--- setupdearquivo/script.js
+++ setupdearquivo/script.js
@@ -1,3 +1,7 @@
 let div=document.querySelector("div")
-let incluiClasse=div.classList.contains("borda-azul")
+let incluiClasse=div.classList.contains("texto-novo")
+div.classList.add("texto-novo");
+div.classList.remove("borda-azul");
+div.classList.toggle("borda-azul");
+div.classList.toggle("borda-azul");
 console.log(incluiClasse);
