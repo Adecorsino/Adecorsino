5ddd4a1f6d574ae8b5130d5265fa19065396ff1a
Adecorsino <adecorsino@gmail.com>
2023-01-02 20:05:01 -0300

    click1

---
+++ click/scrip.js
@@ -0,0 +1,10 @@
+ler button = document.querySelector("button");
+ler span = document.querySelector("span");
+ler section= document.querySelector("section");
+
+function mostrarSpan(){
+    span.style.opacity = "100"
+}
+
+button.addEventListener("mouseover",mostrarSpan);
+
