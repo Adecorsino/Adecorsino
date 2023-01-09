9dfe2cb67d3d8689050320ea908c099ec08b88db
Adecorsino <adecorsino@gmail.com>
2023-01-09 20:22:32 -0300

    tecla1

---
+++ target/script.js
@@ -0,0 +1,8 @@
+let elementoClicado = document.getElementById("elemento-clicado");
+let botaoTempo = document.getElementById("botao-tempo");
+let tempo = document.getElementById("tempo");
+
+botaoTempo.addEventListener("click", function(e){
+    console.log(e.target);
+    
+})
