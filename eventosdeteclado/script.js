b3f3600780faa087677a360a2466edea16df41ee
Adecorsino <adecorsino@gmail.com>
2023-01-09 21:32:08 -0300

    tecla2

---
+++ eventosdeteclado/script.js
@@ -0,0 +1,9 @@
+let keyText = document.querySelector("#key-text");
+let codeText = document.querySelector("#code-text");
+let quadrado = document.querySelector("#quadrado");
+let distanciaDaEsquerda = 0
+
+document.addEventListener("keyup",(e)=>{
+keyText.innertext = e.key;
+codeText.innertext = e.code;
+})
