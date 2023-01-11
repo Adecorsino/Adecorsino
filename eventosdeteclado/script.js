b74bdbed0d09e1eb3e47c2ec1e47c77bfe965362
Adecorsino <adecorsino@gmail.com>
2023-01-11 20:31:43 -0300

    assincrono

--- eventosdeteclado/script.js
+++ eventosdeteclado/script.js
@@ -3,7 +3,13 @@ let codeText = document.querySelector("#code-text");
 let quadrado = document.querySelector("#quadrado");
 let distanciaDaEsquerda = 0
 
-document.addEventListener("keyup",(e)=>{
-keyText.innertext = e.key;
-codeText.innertext = e.code;
-})
+document.addEventListener("keydown",(e)=>{
+    if(e.code == "ArrowRight"){
+        distanciaDaEsquerda = distanciaDaEsquerda +10
+        console.log(distanciaDaEsquerda);
+
+        quadrado.style.left = distanciaDaEsquerda + "px"
+            }
+          })
+           
+    
---
+++ preventdefault()/script.js
@@ -0,0 +1,13 @@
+let linkProz = document.getElementById("link-proz")
+let btnSubmit = document.querySelector("button[type=submit]")
+linkProz.addEventListener("click", (e)=> {
+
+})
+
+btnSubmit.addEventListener("click", (e)=> {
+    e.preventDefault()
+    alert("Nao foi possivel acessar o link");
+  })
+  
+  
+  
