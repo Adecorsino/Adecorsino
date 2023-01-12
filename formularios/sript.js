7ba755e4ba57b0ebbc133bfd4424a0814562d576
Adecorsino <adecorsino@gmail.com>
2023-01-11 21:07:49 -0300

    blur

---
+++ formularios/sript.js
@@ -0,0 +1,8 @@
+let inputEmail = document.getElementById("email");
+let inputIdade = document.getElementById("idade");
+let formulario = document.querySelector("form");
+
+inputEmail.addEventListener("focus", ()=> {
+inputEmail.style.backgroundColor = "lightgreen"
+});
+
